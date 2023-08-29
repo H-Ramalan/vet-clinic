eea9258144ad926b7841da21f08601f561fd4d92
Yonashenok <yonashenok24@gmail.com>
2023-08-29 13:13:12 +0300

    Added index for each table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,3 +3,9 @@ CREATE TABLE medical_histories (id INT PRIMARY KEY, admitted_at TIMESTAMP, patie
 CREATE TABLE treatments (id INT PRIMARY KEY, type VARCHAR(50),name VARCHAR(50));
 CREATE TABLE invoices (id INT PRIMARY KEY, total_amount DECIMAL(10,2), generated_at TIMESTAMP, payed_at TIMESTAMP, medical_history_id INT, FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id));
 CREATE TABLE invoice_items (id INT PRIMARY KEY,unit_price DECIMAL, quantity INT, total_price DECIMAL,invoice_id INT, treatment_id INT, FOREIGN KEY (invoice_id) REFERENCES invoices(id), FOREIGN KEY (treatment_id) REFERENCES treatments(id));
+
+CREATE INDEX patients_asc ON patients(name ASC);
+CREATE INDEX medical_histories_asc ON  medical_histories(status ASC);
+CREATE INDEX treatments_asc ON treatments(name ASC);
+CREATE INDEX invoices_asc ON invoices(total_amount ASC);
+CREATE INDEX invoice_items_asc ON invoice_items(quantity ASC);
