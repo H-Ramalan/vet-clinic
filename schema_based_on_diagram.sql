ed46c0ea88ca242060d56c4d2e85d7f78601e429
Yonashenok <yonashenok24@gmail.com>
2023-08-29 13:43:06 +0300

    Add many-to-many relation of treatements and mediacal histories

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,6 +3,11 @@ CREATE TABLE medical_histories (id INT PRIMARY KEY, admitted_at TIMESTAMP, patie
 CREATE TABLE treatments (id INT PRIMARY KEY, type VARCHAR(50),name VARCHAR(50));
 CREATE TABLE invoices (id INT PRIMARY KEY, total_amount DECIMAL(10,2), generated_at TIMESTAMP, payed_at TIMESTAMP, medical_history_id INT, FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id));
 CREATE TABLE invoice_items (id INT PRIMARY KEY,unit_price DECIMAL, quantity INT, total_price DECIMAL,invoice_id INT, treatment_id INT, FOREIGN KEY (invoice_id) REFERENCES invoices(id), FOREIGN KEY (treatment_id) REFERENCES treatments(id));
+CREATE TABLE medical_histories_treatments (
+treatment_id INT REFERENCES  treatments(id), 
+medical_history_id INT REFERENCES medical_histories(id),
+PRIMARY KEY(treatment_id, medical_history_id) 
+);
 
 CREATE INDEX patients_asc ON patients(name ASC);
 CREATE INDEX medical_histories_asc ON  medical_histories(status ASC);
